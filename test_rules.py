fae5681e93709a468de3c6c4ba2cf2b9db23da44
YSLucas <54868396+YSLucas@users.noreply.github.com>
2020-12-04 15:49:23 +0100

    (RED) back to previous* version of tests
    Terug naar de rode fase omdat de tests die ik had gemaakt niet losstaand zouden werken maar alleen tijdens de simulatie.
    
    * aangepaste versie van een vorige versie

--- test_rules.py
+++ test_rules.py
@@ -19,45 +19,55 @@ class TestRules(TestCase):
     def test_exposure(self):
         """
         Test for exposure rule. 
-        Exposure rule: any alive cell with less than 1 alive neighbour cells dies
+        Exposure rule: any alive cell with less than 2 alive neighbour cells dies
         """
+        x, y = 4, 4
+
+        # set cell to alive
+        self.world.set(x, y, 1)
+        # set neighbour cells according to rule
+        self.world.set(3, 3, 1)
+        self.world.set(3, 4, 0)
+        self.world.set(3, 5, 0)
+        self.world.set(4, 3, 0)
+        self.world.set(4, 5, 0)
+        self.world.set(5, 3, 0)
+        self.world.set(5, 4, 0)
+        self.world.set(5, 5, 0)
+
+        prev_value = self.world.get(x, y)          # get current state of cell
+        self.sim.update()                          # update world once
+        new_value = self.world.get(x, y)           # get new state of cell
+
+        self.assertNotEqual(prev_value, new_value) # check if cell state changed from alive (1) to dead (0)
+
+
+        # Deze versie kan werken maar dan moet de test tijdens de simulatie worden uitgevoerd.
         # get world info
-        width = self.world.width
-        height = self.world.height
-        x, y = None, None
-        prev_value = None
+        # width = self.world.width
+        # height = self.world.height
+        # x, y = None, None
+        # prev_value = None
 
-        for search_x in range(0, width + 1):
-            for search_y in range(0, height + 1):
+        # for search_x in range(0, width + 1):
+        #     for search_y in range(0, height + 1):
 
-                cell_state = self.world.get(search_x, search_y)
-                neighbours = self.world.get_neighbours(search_x, search_y)
+        #         cell_state = self.world.get(search_x, search_y)
+        #         neighbours = self.world.get_neighbours(search_x, search_y)
 
-                if prev_value and ( neighbours.count(1) < 2 ):
-                    x, y, prev_value = search_x, search_y, cell_state
-                    break
-            break
-
-        if prev_value == None:
-            print('No alive cell found') # raise error
-            self.assertEqual(prev_value, 0)
-        else:
-            self.sim.update                            
-            new_value = self.world.get(x, y)          
-            self.assertNotEqual(prev_value, new_value)
-
-
-        # Eerste versie van test. Maar deze test stopt niet voordat het een cel heeft gevonden die voldoet. Kan errors veroorzaken.
-        # while True:
-        #     x, y = random.randint(0, width), random.randint(0, height) # get random x and y in range of board
-        #     prev_value = self.world.get(x, y)
-        #     if prev_value:                                     # check if cell is alive
-        #         neighbours = self.world.get_neighbours(x, y)   # find neighbours of cell at x, y
-        #         if neighbours.count(1) < 2:                    # check if cell has less than 2 alive nieghbours; else get a new cell
-        #             self.sim.update                            # update world
-        #             new_value = self.world.get(x, y)           # get new state of cell at x, y
-        #             self.assertNotEqual(prev_value, new_value) # check if cell changed from alive (1) to dead (0)
+        #         if prev_value and ( neighbours.count(1) < 2 ):
+        #             x, y, prev_value = search_x, search_y, cell_state
         #             break
+        #     break
+
+        # if prev_value == None:
+        #     print('No alive cell found') # raise error
+        #     self.assertEqual(prev_value, 0)
+        # else:
+        #     self.sim.update                            
+        #     new_value = self.world.get(x, y)          
+        #     self.assertNotEqual(prev_value, new_value)
+
 
 
     def test_overcrowding(self):
@@ -65,60 +75,50 @@ class TestRules(TestCase):
         Test for overcrowding rule. 
         Overcrowding rule: any alive cell with more than 3 alive neighbour cells dies
         """
-        # get world info
-        width = self.world.width
-        height = self.world.height
-        x, y = None, None
-        prev_value = None
+        x, y = 4, 4
 
-        for search_x in range(0, width + 1):
-            for search_y in range(0, height + 1):
+        # set cell to alive
+        self.world.set(x, y, 1)
+        # set neighbour cells according to rule
+        self.world.set(3, 3, 1)
+        self.world.set(3, 4, 1)
+        self.world.set(3, 5, 1)
+        self.world.set(4, 3, 1)
+        self.world.set(4, 5, 0)
+        self.world.set(5, 3, 0)
+        self.world.set(5, 4, 0)
+        self.world.set(5, 5, 0)
 
-                cell_state = self.world.get(search_x, search_y)
-                neighbours = self.world.get_neighbours(search_x, search_y)
+        prev_value = self.world.get(x, y)          # get current state of cell
+        self.sim.update()                          # update world once
+        new_value = self.world.get(x, y)           # get new state of cell
 
-                if prev_value and ( neighbours.count(1) > 3 ):
-                    x, y, prev_value = search_x, search_y, cell_state
-                    break
-            break
-
-        if prev_value == None:
-            print('No alive cell found') # raise error
-            self.assertEqual(prev_value, 0)
-        else:
-            self.sim.update                            
-            new_value = self.world.get(x, y)          
-            self.assertNotEqual(prev_value, new_value)
+        self.assertNotEqual(prev_value, new_value) # check if cell state changed from alive (1) to dead (0)
 
     def test_survival(self):
         """
         Test for survival rule. 
         Survival rule: any alive cell with 2 or 3 neighbours stays alive
         """
-        # get world info
-        width = self.world.width
-        height = self.world.height
-        x, y = None, None
-        prev_value = None
+        x, y = 4, 4
 
-        for search_x in range(0, width + 1):
-            for search_y in range(0, height + 1):
+        # set cell to alive
+        self.world.set(x, y, 1)
+        # set neighbour cells according to rule
+        self.world.set(3, 3, 1)
+        self.world.set(3, 4, 1)
+        self.world.set(3, 5, 0)
+        self.world.set(4, 3, 0)
+        self.world.set(4, 5, 0)
+        self.world.set(5, 3, 0)
+        self.world.set(5, 4, 0)
+        self.world.set(5, 5, 0)
 
-                cell_state = self.world.get(search_x, search_y)
-                neighbours = self.world.get_neighbours(search_x, search_y)
+        prev_value = self.world.get(x, y)          # get current state of cell
+        self.sim.update()                          # update world once
+        new_value = self.world.get(x, y)           # get new state of cell
 
-                if prev_value and ( 1 < neighbours.count(1) < 4 ):
-                    x, y, prev_value = search_x, search_y, cell_state
-                    break
-            break
-
-        if prev_value == None:
-            print('No alive cell found') # raise error
-            self.assertEqual(prev_value, 0)
-        else:
-            self.sim.update                            
-            new_value = self.world.get(x, y)          
-            self.assertEqual(prev_value, new_value)
+        self.assertEqual(prev_value, new_value)    # check if cell state stayed alive (1)
 
 
     def test_birth(self):
@@ -126,27 +126,22 @@ class TestRules(TestCase):
         Test for birth rule. 
         Birth rule: any dead cell with exaxtly 3 nieghbours becomes alive
         """
-        # get world info
-        width = self.world.width
-        height = self.world.height
-        x, y = None, None
-        prev_value = None
+        x, y = 4, 4
 
-        for search_x in range(0, width + 1):
-            for search_y in range(0, height + 1):
+        # set cell to dead
+        self.world.set(x, y, 0)
+        # set neighbour cells according to rule
+        self.world.set(3, 3, 1)
+        self.world.set(3, 4, 1)
+        self.world.set(3, 5, 1)
+        self.world.set(4, 3, 0)
+        self.world.set(4, 5, 0)
+        self.world.set(5, 3, 0)
+        self.world.set(5, 4, 0)
+        self.world.set(5, 5, 0)
 
-                cell_state = self.world.get(search_x, search_y)
-                neighbours = self.world.get_neighbours(search_x, search_y)
+        prev_value = self.world.get(x, y)          # get current state of cell
+        self.sim.update()                          # update world once
+        new_value = self.world.get(x, y)           # get new state of cell
 
-                if prev_value and ( neighbours.count(1) == 3 ):
-                    x, y, prev_value = search_x, search_y, cell_state
-                    break
-            break
-
-        if prev_value == None:
-            print('No dead cell found') # raise error
-            self.assertEqual(prev_value, 0)
-        else:
-            self.sim.update                            
-            new_value = self.world.get(x, y)          
-            self.assertNotEqual(prev_value, new_value)
+        self.assertNotEqual(prev_value, new_value) # check if cell state changed from dead (0) to alive (1)
